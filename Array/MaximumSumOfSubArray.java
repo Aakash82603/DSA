2bbc144c82b6d86cd5e4dff57cef4626c1cc5af9
Aakash82603 <24BCS12826@cuchd.in>
2026-07-20 22:52:57 +0530

    Leetcode problem

--- Array/MaximumSumOfSubArray.java
+++ Array/MaximumSumOfSubArray.java
@@ -1,4 +1,35 @@
 package Array;
+import java.util.*;
 
 public class MaximumSumOfSubArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 4, 2, 9, 9, 9};
+        HashSet<Integer> set = new HashSet<>();
+        int k = 3;
+        int maxSum=0;
+
+
+        for(int i=0; i<k; i++){
+            set.add(nums[i]);
+                maxSum += nums[i];
+
+        }
+        int windowSum=0;
+        if(set.size()==k){
+            windowSum=maxSum;
+        }
+        for(int j=k; j<nums.length; j++){
+            set.remove(nums[j-k]);
+            maxSum -= nums[j-k];
+            set.add(nums[j]);
+            maxSum += nums[j];
+            if(set.size()==k){
+                windowSum = Math.max(maxSum, windowSum);
+            }
+        }
+        System.out.println(windowSum);
+    }
 }
+
+
+
