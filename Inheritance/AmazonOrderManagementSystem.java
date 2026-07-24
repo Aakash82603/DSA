e6a92114c9851c20b1ea95a165ba79d9286c9b95
Aakash82603 <24BCS12826@cuchd.in>
2026-07-25 01:53:04 +0530

    Inheritance practice

--- Inheritance/AmazonOrderManagementSystem.java
+++ Inheritance/AmazonOrderManagementSystem.java
@@ -1,4 +1,74 @@
 package Inheritance;
+class Product{
+    int productId;
+    String productName;
+    String brand;
+    double price;
+
+    Product(int productId,String productName,String brand,double price){
+        this.productId=productId;
+        this.productName=productName;
+        this.brand=brand;
+        this.price=price;
+    }
+    public void displayProductdetails(){
+        System.out.println("Product Id: "+productId);
+        System.out.println("Product Name: "+productName);
+        System.out.println("Brand: "+brand);
+        System.out.println("Price: "+price);
+    }
+}
+class Electronics extends Product{
+    int warrantyPeriod;
+    int powerconsumption;
+
+    Electronics(int productId,String productName,String brand,double price,int warrantyPeriod,int powerconsumption){
+        super(productId,productName,brand,price);
+        this.warrantyPeriod=warrantyPeriod;
+        this.powerconsumption=powerconsumption;
+    }
+    public void showWarranty(){
+        System.out.println("Warranty Period: "+warrantyPeriod+" months");
+        System.out.println("Power Consumption: "+powerconsumption+" watts");
+    }
+}
+class Clothing  extends Product{
+    int size;
+    String fabricType;
+    Clothing(int productId,String productName,String brand,double price,int size,String fabricType){
+        super(productId,productName,brand,price);
+        this.size=size;
+        this.fabricType=fabricType;
+    }
+    public void showSize(){
+        System.out.println("Size of Clothing: "+size);
+        System.out.println("Fabric Type: "+fabricType);
+    }
+}
+class Grocery  extends Product{
+    int expiryDate;
+    int weight;
+    Grocery(int productId,String productName,String brand,double price,int weight,int expiryDate){
+        super(productId,productName,brand,price);
+        this.weight=weight;
+        this.expiryDate=expiryDate;
+    }
+    public void checkExpiry(){
+        System.out.println("Weight of Grocery: "+weight+" grams");
+        System.out.println("Expiry Date: "+expiryDate);
+    }
+}
 
 public class AmazonOrderManagementSystem {
+    public static void main(String[] args) {
+        Electronics E=new Electronics(101,"Smartphone","Samsung",50000,24,15);
+        E.displayProductdetails();
+        E.showWarranty();
+        Clothing C=new Clothing(102,"T-Shirt","Nike",2000,42,"Cotton");
+        C.displayProductdetails();
+        C.showSize();
+        Grocery G=new Grocery(103,"Milk","Amul",50,1000,2024);
+        G.displayProductdetails();
+        G.checkExpiry();
+    }
 }
--- Inheritance/UniversityManagementSystem.java
+++ Inheritance/UniversityManagementSystem.java
@@ -1,4 +1,60 @@
 package Inheritance;
+class parent{
+    String name;
+    int age;
+    long phnumber;
+    parent(String name,int age,long phnumber){
+        this.name=name;
+        this.age=age;
+        this.phnumber=phnumber;
+    }
+    public void displayInfo() {
+        System.out.println("Name" + name);
+        System.out.println("Age: " + age);
+        System.out.println("Phnumber: " + phnumber);
+    }
+}
+class Student extends parent{
+    String course;
+    Student(String  name,int age,long phnumber ,String course){
+        super(name, age, phnumber);
+        this.course=course;
+    }
+   public void attentdClass(){
+        System.out.println(name + " is attending class for " + course);
 
+   }
+}
+class Professor extends parent{
+    String subject;
+    Professor(String  name,int age,long phnumber ,String subject){
+        super(name, age, phnumber);
+        this.subject=subject;
+    }
+    public void teachClass(){
+        System.out.println(name + " is teaching class for " + subject);
+    }
+}
+class staff extends parent{
+    String department;
+    staff(String name,int age,long phnumber ,String department){
+        super(name, age, phnumber);
+        this.department=department;
+    }
+    public void manageOffice(){
+        System.out.println(name + " is managing office for " + department);
+    }
+}
 public class UniversityManagementSystem {
+    public static void main(String[] args) {
+      Student s=new Student("Aakash",20,1234567890,"Computer Science");
+      Professor p=new Professor("Dr. Smith",45,9876543210l,"Mathematics");
+      staff st=new staff("John Doe",35,5555555555l,"IT Department");
+      s.displayInfo();
+      s.attentdClass();
+      p.displayInfo();
+      p.teachClass();
+      st.displayInfo();
+      st.manageOffice();
+    }
 }
